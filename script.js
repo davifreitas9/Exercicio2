3082310c5e311ebe74c0b3d1fe31574dd8ee8664
Davi <davi.silva@novaandradina.org>
2020-04-20 02:09:15 -0300

    resolucao completa

---
+++ script.js
@@ -0,0 +1,20 @@
+function isEven(value) {
+		if (value%2 == 0)
+			return true;
+		else
+			return false;
+}
+
+//funcao para numeros negativos abaixo:
+//----------------------------------------
+// function isEven(value) {
+//	if(value >= 0){
+//		if (value%2 == 0)
+//			return true;
+//		else
+//			return false;
+//	}
+//}
+//-----------------------------------------
+
+console.log(isEven(50));
